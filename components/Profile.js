18cf8a6cc6e69dce9e9d2c8f8ef8fcce6db9b64e
aileenwrozyna <130551145+aileenwrozyna@users.noreply.github.com>
2023-04-13 20:16:06 +0200

    cloudinary url update

--- components/Profile.js
+++ components/Profile.js
@@ -9,7 +9,7 @@ function Profile({ content }) {
       <div className="profile-card">
         <img
           className="profile-card__image"
-          src={`${process.env.NEXT_PUBLIC_STRAPI_URL}${content.picture.data.attributes.url}`}
+          src={`content.picture.data.attributes.url`}
           alt="Photo of Aileen"
         />
         <div className="profile-card__description">
