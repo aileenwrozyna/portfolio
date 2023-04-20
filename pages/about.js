b556d60efa403dc3209b2d089cc5c217627aa0f9
aileenwrozyna <130551145+aileenwrozyna@users.noreply.github.com>
2023-04-20 20:10:46 +0200

    dynamic footer update

--- pages/about.js
+++ pages/about.js
@@ -1,29 +1,37 @@
 import axios from "axios";
 import Profile from "../components/Profile";
+import Footer from "../components/Footer";
 
-function About({ data }) {
-	const dataContent = data.data[0].attributes;
+function About({ data, footer }) {
+  const dataContent = data.data[0].attributes;
 
-	return (
-		<>
-		  <div className="wrapper">
-		    <Profile content={dataContent} />
-		  </div>
-		</>
-	)
+  return (
+    <>
+      <main className="main">
+        <div className="wrapper">
+          <Profile content={dataContent} />
+        </div>
+      </main>
+      <Footer text={footer.data[0].attributes.about} />
+    </>
+  );
 }
 
 export default About;
 
 export async function getServerSideProps() {
-	const data = await axios.get(
-	  `${process.env.NEXT_PUBLIC_STRAPI_URL}/api/abouts?populate=*`
-	);
-  
-	return {
-	  props: {
-		data: data.data,
-	  },
-	};
-  }
-  
+  const data = await axios.get(
+    `${process.env.NEXT_PUBLIC_STRAPI_URL}/api/abouts?populate=*`
+  );
+
+  const footer = await axios.get(
+    `${process.env.NEXT_PUBLIC_STRAPI_URL}/api/footers`
+  );
+
+  return {
+    props: {
+      data: data.data,
+      footer: footer.data
+    },
+  };
+}
